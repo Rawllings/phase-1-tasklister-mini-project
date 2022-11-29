8750a704cde2cea9893a383e28ae3925eda19376
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-29 15:08:37 +0300

    Lister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", function(){
+
+  let form = document.querySelector("form")
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    doList(e.target.new.value)
+    form.reset()
+  })
+
+
+})
+
+function doList(list) {
+let li = document.createElement("li")
+let btn = document.createElement("button")
+li.textContent = `${list}`
+btn.textContent = "X"
+document.querySelector("#tasks").appendChild(li)
+li.appendChild(btn)
+btn.addEventListener("click", deletebtn)
+}
+
+function deletebtn(e) {
+  e.target.parentNode.remove()
+}
